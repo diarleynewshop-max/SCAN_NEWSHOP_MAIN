b631d894789e40a2dccf0123aee328288dfa1ea6
diarleynewshop-max <diarley.newshop@gmail.com>
2026-04-02 11:00:03 -0300

    main.tsx arrumado

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// 1. Importe o ThemeProvider que criamos
-import { ThemeProvider } from "./components/theme-provider"; 
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
-  // 2. Envolva o App com o ThemeProvider
   <ThemeProvider defaultTheme="system" storageKey="newshop-theme">
     <App />
   </ThemeProvider>
