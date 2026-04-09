c2e9030db235d98a532d80ed958053a13ce8b6a5
diarleynewshop-max <diarley.newshop@gmail.com>
2026-04-09 14:06:43 -0300

    kssksk

--- src/pages/Compras.tsx
+++ src/pages/Compras.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { ProductImage } from "@/components/ProductImage";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
