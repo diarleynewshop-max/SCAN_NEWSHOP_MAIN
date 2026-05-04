e74da485474b0b8cc09286f963a02de144af3cf6
diarleynewshop-max <diarley.newshop@gmail.com>
2026-05-04 13:32:57 -0300

    trigger

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
+import { additionalPackages } from "@trigger.dev/build/extensions/core";
 
 export default defineConfig({
   project: "proj_mvardqmhhvljrkfmiuqg",
   runtime: "node",
   dirs: ["./src/trigger"],
   maxDuration: 5,
+  build: {
+    external: ["sharp"],
+    extensions: [
+      additionalPackages({
+        packages: ["sharp"],
+      }),
+    ],
+  },
 });
