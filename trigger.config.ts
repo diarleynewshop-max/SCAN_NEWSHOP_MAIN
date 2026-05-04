c747c07097597d17bd02aa48a0a4cc26904d95ca
diarleynewshop-max <diarley.newshop@gmail.com>
2026-05-04 13:40:05 -0300

    fix trigger sharp external

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
-import { additionalPackages } from "@trigger.dev/build/extensions/core";
 
 export default defineConfig({
   project: "proj_mvardqmhhvljrkfmiuqg",
@@ -8,10 +7,5 @@ export default defineConfig({
   maxDuration: 5,
   build: {
     external: ["sharp"],
-    extensions: [
-      additionalPackages({
-        packages: ["sharp"],
-      }),
-    ],
   },
 });
