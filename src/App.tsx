4e913a77141ecdfb4020692df08b6c9ec6b428f7
diarleynewshop-max <diarley.newshop@gmail.com>
2026-06-05 09:23:40 -0300

    vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Suspense, lazy } from "react";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -100,6 +101,7 @@ const App = () => (
           </Routes>
         </Suspense>
       </BrowserRouter>
+      <VercelAnalytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
